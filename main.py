8dceebbc5ebee124ea461a225fd06efbf8528594
andrii <andrijkorolevic@gmail.com>
2022-09-27 19:39:49 +0300

    Add new branch with lab

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Yeaahhh")
+print("Введіть діаметр")
+di = int(input("->"))
+print("1 - площа / 2 - периметр")
+ch = int(input("->"))
+
+if ch == 1 :
+    res1 = (3.14 / 4) * (di * di)
+    print(f"{res1}")
+
+elif ch == 2 :
+    res2 = 3.14 * di
+    print(f"{res2}")
