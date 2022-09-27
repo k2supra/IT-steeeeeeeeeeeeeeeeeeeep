b343edc31459b92de2fb82b60d9d7945df9f9423
andrii <andrijkorolevic@gmail.com>
2022-09-27 20:16:22 +0300

    Did very strong work))

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Yeaahhh")
+time = int(input("->"))
+
+if time > 0 and time < 6 :
+    print("Good Night")
+
+if time > 5 and time < 13 :
+    print("Good Morning")
+
+if time > 12 and time < 17 :
+    print("Good Day")
+
+if time > 16 and time < 25 :
+    print("Good Evening")
