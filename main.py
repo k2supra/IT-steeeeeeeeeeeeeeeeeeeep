edf3b1eab939310c04b61b3e0520bf91bc41c643
andrii <andrijkorolevic@gmail.com>
2022-09-27 19:53:59 +0300

    Did strong work)

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Yeaahhh")
+print("Введіть вартість пристаки")
+m1 = int(input("->"))
+
+print("Введіть їх кількість")
+m2 = int(input("->"))
+
+print("Введіть знижку %")
+m3 = int(input("->"))
+
+print("1 - сумма всіх приставок / 2 - вартість 1 приставки")
+base = int(input("->"))
+
+part = (m3 / 100) * m1
+part_1 = (m1 - part) * m2
+part_2 = part_1 / m2
+if base == 1 :
+    print(f"{part_1}")
+
+if base == 2 :
+    print(f"{part_2}")
